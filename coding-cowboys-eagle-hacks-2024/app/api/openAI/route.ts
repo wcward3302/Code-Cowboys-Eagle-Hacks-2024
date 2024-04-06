5cba7f9dd4a3e9df0f4731a48f537e3e41617c8a
William Murphy <williamtrumurphy@gmail.com>
2024-04-05 22:36:01 -0400

    Alert Prompt for Corrected Output.

--- coding-cowboys-eagle-hacks-2024/app/api/openAI/route.ts
+++ coding-cowboys-eagle-hacks-2024/app/api/openAI/route.ts
@@ -10,7 +10,11 @@ export async function POST(req: Request, res: NextResponse) {
 
   const system_message = {
     role: "system",
-    content:  "You are an assistant, if the user asks to create an invoice, you need to get (name, email, amount, status (paid or pending), date (ensure it is the format of: yyyymmdd)), if the return information does not match all fields, ask user again for missing information." 
+    content:  `You are an invoice managing assistant, if the user asks to create an invoice, 
+                you only need to get (name, email, amount, status (paid or pending), date (ensure it is the format of: yyyymmdd)), 
+                if the return information does not match all fields, ask user again for missing information.
+                Once you have all the information, only thank the user, tell them their invoice was created, display the information and 
+                make sure the last world is banana with a period. If prompted again, treat this as a new invoice and go through the process again.`
   }
 
   body.messages.push(system_message);
